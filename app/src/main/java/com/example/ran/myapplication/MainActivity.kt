73299fd18b9732605db0218fd152beb5671de79d
BouguesriRania <hr_bouguesri@esi.dz>
2021-04-11 11:32:55 +0100

    2nd commit

--- app/src/main/java/com/example/ran/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/ran/myapplication/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.ran.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Build.MODEL
+import android.os.Build
+
 
 
 
@@ -11,4 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+}
+
+fun getDeviceName(): String {
+    val manufacturer = Build.MANUFACTURER
+    val model = Build.MODEL
+    return if (model.startsWith(manufacturer)) {
+        model
+    } else "$manufacturer $model"
 }
